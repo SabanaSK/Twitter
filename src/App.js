4f66138c3cb6d18bde43ed1776fb28cac16b16ea
Natalie <natalie.hallerdal@yahoo.se>
2023-04-25 15:54:07 +0200

    adding edit side to the project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import StartPage from './Components/StartPage/StartPage';
 import LoginPage from './Components/LogInLogOut/LoginPage';
 import RegisterUser from './Components/RegistreUser/RegisterUser';
-import ProfilePage from './Components/ProfilePage/ProfilePage';
+import ProfilePage from './Components/ProfilePages/MyProfilePage';
+import EditPage from './Components/ProfilePages/EditPage'
 import LogOut from './Components/LogInLogOut/LogOut';
 
 
@@ -16,7 +17,8 @@ function App ()
         <Route path="/" element={<StartPage />} />
         <Route path="/login" exact element={<LoginPage />} />
         <Route path="/register" exact element={<RegisterUser onRegisterUser />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/edit" element={<EditPage />} />
         <Route path="/logout" element={<LogOut />} />
       </Routes>
     </>
