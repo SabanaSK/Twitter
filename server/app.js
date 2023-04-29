4caf7af277ed81778d3ddc7b1de56a859900c4e3
SabanaSK <sornkarn_su@hotmail.com>
2023-04-29 13:16:13 +0200

    Post Frontend
    Can now do the post from frontend, backend is also done
    But still cannot see the post in frontend

--- server/app.js
+++ server/app.js
@@ -10,8 +10,7 @@ import dbConnect from "./db/database.js";
 const app = express();
 dbConnect();
 
-app.use((req, res, next) =>
-{
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -27,69 +26,56 @@ app.use((req, res, next) =>
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response, next) =>
-{
+app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
 });
 
-app.get("/users", async (request, response) =>
-{
-  try
-  {
+app.get("/users", async (request, response) => {
+  try {
     const users = await User.find({});
     response.status(200).send(users);
-  } catch (error)
-  {
+  } catch (error) {
     console.log(error);
     response.status(500).send("Internal server error");
   }
 });
-app.get("/homeuser", async (request, response) =>
-{
-  try
-  {
-    const users = await User.aggregate([ { $sample: { size: 4 } } ]);
+app.get("/homeuser", async (request, response) => {
+  try {
+    const users = await User.aggregate([{ $sample: { size: 4 } }]);
     response.status(200).send(users);
-  } catch (error)
-  {
+  } catch (error) {
     console.log(error);
     response.status(500).send("Internal server error");
   }
 });
 
 
-app.get("/profile/:id", async (request, response) =>
-{
-  try
-  {
+app.get("/profile/:id", async (request, response) => {
+  try {
     const user = await User.findById(request.params.id);
     if (!user) {
       response.status(404).send("The user was not found.");
     } else {
       response.status(200).send(user);
     }
-  } catch (error)
-  {
+  } catch (error) {
     console.log(error);
     response.status(500).send("Internal server error");
   }
 });
 
-app.post("/profile/:id", async (req, res) =>
-{
+app.post("/profile/:id", async (req, res) => {
   const { id } = req.params;
   const updatedProfile = req.body;
 
-  try
-  {
+  try {
     // TODO: Update the user profile in the database with the new data
     // ...
 
     // Return a success response
     res.status(200).send('Profile updated successfully');
-  } catch (err)
-  {
+  } catch (err) {
     // Return an error response if something went wrong
     console.error(err);
     res.status(500).send('Error updating profile');
@@ -97,31 +83,26 @@ app.post("/profile/:id", async (req, res) =>
 })
 
 
-app.post("/register", async (request, response) =>
-{
-  try
-  {
+app.post("/register", async (request, response) => {
+  try {
     const hashPassword = await bcrypt.hash(request.body.password, 10);
     const user = new User({
       username: request.body.username,
       email: request.body.email,
       password: hashPassword,
     });
-    try
-    {
+    try {
       const result = await user.save();
       response.status(201).send({
         message: "User registered successfully",
       });
-    } catch (error)
-    {
+    } catch (error) {
       console.log("Something went wrong", error);
       response.status(400).send({
         message: "Could not register user",
       });
     }
-  } catch (error)
-  {
+  } catch (error) {
     response.status(500).send({
       message: "Server Error",
     });
@@ -129,19 +110,15 @@ app.post("/register", async (request, response) =>
 });
 
 
-app.post("/login", async (request, response) =>
-{
-  const user = await User.findOne({ $or: [ { email: request.body.email }, { username: request.body.username } ] });
-  if (user)
-  {
+app.post("/login", async (request, response) => {
+  const user = await User.findOne({ $or: [{ email: request.body.email }, { username: request.body.username }] });
+  if (user) {
     const match = await bcrypt.compare(request.body.password, user.password);
-    if (!match)
-    {
+    if (!match) {
       response.status(404).send({
         message: "Bad Request",
       });
-    } else
-    {
+    } else {
       const token = jwt.sign(
         {
           userId: user._id,
@@ -157,25 +134,31 @@ app.post("/login", async (request, response) =>
         token,
       });
     }
-  } else
-  {
+  } else {
     response.status(404).send({
       message: "Bad Request",
     });
   }
 });
 
-app.post("/tweet", authMiddleware, async (request, response) =>
-{
-  try
-  {
+app.get("/tweets", async (req, res) => {
+  try {
+    const tweets = await Post.find({});
+    res.status(200).send(tweets);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.post("/tweet", authMiddleware, async (request, response) => {
+  try {
     const { userId } = request.user;
     const { text } = request.body;
     const tweet = new Post({ author: userId, text });
     const savedTweet = await tweet.save();
     response.status(201).send(savedTweet);
-  } catch (error)
-  {
+  } catch (error) {
     console.log(error);
     response.status(500).send("Internal server error");
   }
