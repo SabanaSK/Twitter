80089a5bbe828c21bb6d7b59b95e7effa064d0b7
Nandini <patilnandini1811@gmail.com>
2023-05-02 11:20:14 +0200

    Test otheruserprofile

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import StartPage from './Components/StartPage/StartPage';
 import LoginPage from './Components/LogInLogOut/LoginPage';
 import RegisterUser from './Components/RegistreUser/RegisterUser';
-import ProfilePage from './Components/ProfilePages/MyProfilePage';
+// import ProfilePage from './Components/ProfilePages/MyProfilePage';
 import EditPage from './Components/ProfilePages/EditPage'
 import LogOut from './Components/LogInLogOut/LogOut';
 import Home from './Components/Home/Home'
+import OtherUserProfilePage from './Components/OtherUsersProfiles/OtherUserProfile';
 
 
 function App ()
@@ -19,6 +20,8 @@ function App ()
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" exact element={<LoginPage />} />
         <Route path="/register" exact element={<RegisterUser onRegisterUser />} />
+        <Route path="/users/:id" exact element={<OtherUserProfilePage />} />
+
         <Route path="/edit/:id" element={<EditPage />} />
         <Route path="/logout" element={<LogOut />} />
         <Route path="/home/*" element={<Home />} />
