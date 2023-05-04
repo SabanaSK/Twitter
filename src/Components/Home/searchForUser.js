19115f5509facb28719efc7fa5ee4bf858fd9af5
Natalie <natalie.hallerdal@yahoo.se>
2023-05-04 13:44:37 +0200

    search field done

--- src/Components/Home/searchForUser.js
+++ src/Components/Home/searchForUser.js
@@ -1,38 +1,64 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SearchForFriends = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [allUsers, setallUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
+  const [searchStarted, setSearchStarted] = useState(false);
 
   const fetchAllUsers = async (searchQuery) => {
     try {
-      const response = await axios.get(`http://localhost:3001/users?search=${searchQuery}`);
-      setallUsers(response.data);
+      const response = await axios.get(`http://localhost:3001/search?search=${searchQuery}`);
+      setAllUsers(response.data);
     } catch (error) {
       console.log(error.message);
     }
   };
 
   useEffect(() => {
-    fetchAllUsers(searchQuery);
-  }, [searchQuery]);
+    if (searchStarted) {
+      fetchAllUsers(searchQuery);
+    }
+  }, [searchQuery, searchStarted]);
+
+  const filteredUsers = allUsers.filter((user) =>
+    user.username.toLowerCase().startsWith(searchQuery.toLowerCase())
+  );
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    setSearchStarted(true);
+  };
+
+  
 
   return (
     <div>
       <input
         type="text"
         value={searchQuery}
-        onChange={(event) => setSearchQuery(event.target.value)}
+        onChange={handleInputChange}
         placeholder="Search on Twitter..."
       />
+
       <ul>
-        {allUsers.map((user) => (
-          <li key={user.id}>{user.name}</li>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.username}</Link>
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default SearchForFriends;   
+
+
+export default SearchForFriends;
+
+
+
+
+
